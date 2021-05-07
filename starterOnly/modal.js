1effbad8b9a430161a2c948697f2f1af609b54dd
MecyDev <macmc@tutanota.com>
2021-05-07 11:33:08 +0200

    Feature added

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,20 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalX = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Close modal event
+modalX.forEach((close) => close.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+//close the modal
+function closeModal() {
+  modalbg.style.display = "none";
+}
