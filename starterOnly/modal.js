d39dcf8284174c0795d8b20874f0622a1566be09
MecyDev <macmc@tutanota.com>
2021-05-12 09:16:53 +0200

    error messages terminé. virification dans fonction validate

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -28,3 +28,43 @@ function launchModal() {
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+/*function errorVisible(e) {
+  if (e === false) {
+    formData[0].setAttribute("data-error-visible", "true");
+    console.log(e);
+  } else {
+    formData[0].setAttribute("data-error-visible", "false");
+  }
+}*/
+
+/*document.querySelector("#first").addEventListener("submit", function (e) {
+  if (document.querySelector("#first").validity.valid === false) {
+    formData[0].setAttribute("data-error-visible", "true");
+  } else {
+    formData[0].setAttribute("data-error-visible", "false");
+  }
+});*/
+
+/*document.querySelector("#first").addEventListener("invalid", function (e) {
+  e.preventDefault();
+});*/
+
+function validate() {
+  const x = document.querySelectorAll("input");
+
+  x.forEach(function (e) {
+    if (e.id != "checkbox2") {
+      if (e.validity.valid === false) {
+        e.parentElement.setAttribute("data-error-visible", "true");
+      } else {
+        e.parentElement.setAttribute("data-error-visible", "false");
+      }
+    }
+  });
+
+  /*modalbg.style.display = "block";
+  document.querySelector("form").style.display = "none";
+  console.log("OKOKOK");*/
+  return false;
+}
